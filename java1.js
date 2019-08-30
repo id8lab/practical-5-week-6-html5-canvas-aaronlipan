ff26d31cbc40bda69480612e57c93ce7792d35d8
aaronlipan <53068359+aaronlipan@users.noreply.github.com>
2019-08-30 13:10:40 +0800

    prac5

---
+++ java1.js
@@ -0,0 +1,9 @@
+var canvas =document.getElementById("mycvs");
+var ctx = canvas.getContext("2d");
+   
+function drawOnload(){
+    ctx.fillstyle= 'red';
+    ctx.fillRect(20,20,100,50);
+   
+ }
+    
